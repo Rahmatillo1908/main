4396189e453721f75f92e87bafbf222b790bd89d
rov1dev <rahmatillo722022@gmail.com>
2024-04-19 10:11:26 +0500

    main

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
-
+import Home from "./components/Home"
+import Navbar from "./components/Navbar"
+import { BrowserRouter,Route,Routes } from "react-router-dom";
 function App() {
   return (
-    <div className="App">
-     
+    <div>
+      <BrowserRouter>
+      <Navbar/>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/teachers" element={<Home/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
